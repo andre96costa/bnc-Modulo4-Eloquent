4993ad3786391ef4512129bb1fbce10ed4af3192
Andre Costa <andre2396costa@gmail.com>
2025-01-14 21:05:05 -0300

    Attributes

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -21,5 +22,42 @@ class Plan extends Model
     public function signatures(): HasMany
     {
         return $this->hasMany(Signature::class);
+    }
+
+    /**
+     * Interact with the  attribute.
+     *
+     * return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    public function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn ( $value) => ucfirst($value),
+        );
+    }
+
+    /**
+     * Interact with the  attribute.
+     *
+     * return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    public function cod(): Attribute
+    {
+        return Attribute::make(
+            get: fn ( $value) => strtoupper($value),
+            set: fn ( $value) => strtolower($value),
+        );
+    }
+
+    /**
+     * Interact with the  attribute.
+     *
+     * return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    public function fullname(): Attribute
+    {
+        return Attribute::make(
+            get: fn ( $value, $attributes) => $attributes['name'] . '-' . $attributes['cod'],
+        );
     }
 }
